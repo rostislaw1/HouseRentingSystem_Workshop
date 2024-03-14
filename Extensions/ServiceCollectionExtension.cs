17078fbdd946e248e4c2dc11b531ca6b865d0173
rostislaw1 <rostislavv.24@gmail.com>
2024-03-14 21:43:36 +0200

    ServiceCollectionExtension added

---
+++ Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,40 @@
+﻿using HouseRentingSystem.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem_Workshop.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicatoinService(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddApplicatoinDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicatoinIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services
+                .AddDefaultIdentity<IdentityUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                })
+              .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddControllersWithViews();
+            return services;
+        }
+    }
+}
